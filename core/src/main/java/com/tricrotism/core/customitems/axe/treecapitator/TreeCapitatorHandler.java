a1ede49acfa77a5cbc0a4be639c50eb9c3b6951a
Sage <tricrotismbusiness@gmail.com>
2024-04-17 13:57:48 -0400

    updated TreeCapitatorHandler code

--- core/src/main/java/com/tricrotism/core/customitems/axe/treecapitator/TreeCapitatorHandler.java
+++ core/src/main/java/com/tricrotism/core/customitems/axe/treecapitator/TreeCapitatorHandler.java
@@ -1,11 +1,15 @@
 package com.tricrotism.core.customitems.axe.treecapitator;
 
 import com.tricrotism.core.utils.ItemUtils;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.meta.tags.ItemTagType;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class TreeCapitatorHandler implements Listener {
 
@@ -17,36 +21,40 @@ public class TreeCapitatorHandler implements Listener {
     @EventHandler
     public void handleTreeCapitatior(BlockBreakEvent event) {
         if (ItemUtils.getCustomTag(event.getPlayer().getInventory().getItemInMainHand(), TreeCapitatorItem.NAMESPACE_KEY, ItemTagType.STRING).equals("true")) {
-            handleTreeBreak(event.getBlock());
+            handleTreeBreak(event.getBlock().getLocation());
         }
     }
 
-    public void handleTreeBreak(Block block) {
-        if (!block.getType().toString().contains("LOG")) {
-            return;
-        }
+    /**
+     * Handles the breaking of a tree.
+     *
+     * @param startLocation the location of the block that was broken.
+     */
+    private void handleTreeBreak(Location startLocation) {
+        Set<Block> visited = new HashSet<>();
+        Set<Block> queue = new HashSet<>();
 
-        block.breakNaturally();
+        queue.add(startLocation.getBlock());
 
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                for (int z = -1; z <= 1; z++) {
-                    if (x == 0 && y == 0 && z == 0) continue;
-                    Block relative = block.getRelative(x, y, z);
-                    if (relative.getType().toString().contains("LOG")) {
-                        handleTreeBreak(relative);
+        while (!queue.isEmpty()) {
+            Block currentBlock = queue.iterator().next();
+            queue.remove(currentBlock);
+            visited.add(currentBlock);
+
+            for (int xOffset = -1; xOffset <= 1; xOffset++) {
+                for (int yOffset = -1; yOffset <= 1; yOffset++) {
+                    for (int zOffset = -1; zOffset <= 1; zOffset++) {
+                        Block adjacentBlock = currentBlock.getRelative(xOffset, yOffset, zOffset);
+                        if (adjacentBlock.getType().toString().contains("LOG") || adjacentBlock.getType().toString().contains("LEAVES")) {
+                            if (!visited.contains(adjacentBlock)) {
+                                queue.add(adjacentBlock);
+                                visited.add(adjacentBlock);
+                                adjacentBlock.breakNaturally();
+                            }
+                        }
                     }
                 }
             }
-        }
-
-        Block above = block.getRelative(0, 1, 0);
-        Block below = block.getRelative(0, -1, 0);
-        if (above.getType().toString().contains("LOG")) {
-            handleTreeBreak(above);
-        }
-        if (below.getType().toString().contains("LOG")) {
-            handleTreeBreak(below);
         }
     }
 
